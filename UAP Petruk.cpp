2f1704df7c250ac3e8479df955ce5f84eaad4b85
TASYA AZZAHRA PUTRI <92432388+tasyaazzahraputri@users.noreply.github.com>
2022-06-15 10:13:33 +0700

    Update UAP Petruk.cpp

--- UAP Petruk.cpp
+++ UAP Petruk.cpp
@@ -22,4 +22,14 @@ int main(){
 	int pilihan,save,pilM;
 	string list[7]={" Bigbang - BaeBae"," Bigbang - Blue"," Bigbang - stil life"," Itzy - Loco"," itzy - not shy"," Twice - TT"," itzy - icy"};
 	queue<string> song;
+	
+	do{
+	Pilihan(list);
+	
+	cout<<"\n Pilih : ";
+	cin>>pilM;
+	
+	
+		
+	}while(pilM != 5);
 }
