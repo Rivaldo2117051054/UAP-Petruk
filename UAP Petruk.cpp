345e6678de0a6ab3e8a9a44e1f262f2ce9119ae3
Rivaldo <92324602+Rivaldo2117051054@users.noreply.github.com>
2022-06-15 08:30:55 +0700

    Update UAP Petruk.cpp

--- UAP Petruk.cpp
+++ UAP Petruk.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 
 int main(){
-	
-	cout<<" >//< ";
+
+	int pilihan,save,pilM;
+	string list[7]={" Bigbang - BaeBae"," Bigbang - Blue"," Bigbang - stil life"," Itzy - Loco"," itzy - not shy"," Twice - TT"," itzy - icy"};
+	queue<string> song;
 }
