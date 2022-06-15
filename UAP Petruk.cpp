a6fbc3eda30550e8649c3d5fcde14420ff523d79
TASYA AZZAHRA PUTRI <92432388+tasyaazzahraputri@users.noreply.github.com>
2022-06-15 09:09:03 +0700

    Update UAP Petruk.cpp

--- UAP Petruk.cpp
+++ UAP Petruk.cpp
@@ -3,6 +3,20 @@
 
 using namespace std;
 
+void Pilihan (string list[]){
+	cout << "\n ========================================\n";
+	cout << "| 1. Masukan music      4. Play music  | \n";
+	cout << "| 2. Tampilkan music    5. 5. Exit     | \n";
+	cout << "| 3. Jumlah Music                      | \n";
+	cout << " ========================================\n";
+	
+	cout<<" Song List "<<endl;
+	
+	for(int i=0; i<7; i++){
+		cout<<" "<<i+1<<"."<<list[i]<<endl;
+	}
+}
+
 int main(){
 
 	int pilihan,save,pilM;
